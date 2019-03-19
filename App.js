87b6166ebf8b0f888791ff0a531c56bda27c1954
idanlo <idan2110@gmail.com>
2019-03-19 15:55:07 +0200

    create basic navigation screens

--- App.js
+++ App.js
@@ -1,89 +1,36 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { TouchableNativeFeedback } from 'react-native-gesture-handler';
-import Spotify from 'rn-spotify-sdk';
+import LoginScreen from './screens/Login';
+import HomeScreen from './screens/Home';
+import SearchScreen from './screens/Search';
+import LibraryScreen from './screens/Library';
+import {
+    createSwitchNavigator,
+    createAppContainer,
+    createBottomTabNavigator
+} from 'react-navigation';
 
-export default class App extends Component {
-    componentDidMount() {
-        this.initializeIfNeeded().catch(err => {
-            console.warn('Error');
-        });
-    }
-
-    async initializeIfNeeded() {
-        if (!(await Spotify.isInitializedAsync())) {
-            // init
-            const options = {
-                clientID: '4266b38056c54d47a5480dc099f59cb6',
-                sessionUserDefaultsKey: 'SpotifySession',
-                redirectURL: 'spotify://auth',
-                scopes: [
-                    'user-read-private',
-                    'playlist-read',
-                    'playlist-read-private',
-                    'streaming'
-                ]
-            };
-
-            const loggedIn = await Spotify.initialize(options);
-
-            if (loggedIn) {
-                console.warn('Success');
-            }
-        } else {
-            if (await Spotify.isLoggedInAsync()) {
-                console.warn('Success');
-            }
+const HomeNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen
+        },
+        Search: {
+            screen: SearchScreen
+        },
+        Library: {
+            screen: LibraryScreen
         }
-    }
-
-    login() {
-        Spotify.login().then(loggedIn => {
-            if (loggedIn) {
-                console.warn('SUCCESS');
-            } else {
-                console.error('FAILURE');
-            }
-        });
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.loginHeader}>Spotify</Text>
-                <TouchableNativeFeedback
-                    style={styles.login}
-                    onPress={this.login}
-                >
-                    <Text style={styles.loginText}>Login</Text>
-                </TouchableNativeFeedback>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#191414'
     },
-    loginHeader: {
-        fontSize: 34,
-        color: '#fff',
-        marginBottom: 25,
-        fontWeight: 'bold'
+    { initialRouteName: 'Home' }
+);
+
+const AppNavigator = createSwitchNavigator(
+    {
+        Login: {
+            screen: LoginScreen
+        },
+        Home: HomeNavigator
     },
-    login: {
-        backgroundColor: '#1DB954',
-        width: 120,
-        height: 60,
-        borderRadius: 60,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    loginText: {
-        color: '#fff'
-    }
-});
+    { initialRouteName: 'Login' }
+);
+
+export default createAppContainer(AppNavigator);
---
+++ screens/Login.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableNativeFeedback } from 'react-native';
+import Spotify from 'rn-spotify-sdk';
+
+export default class LoginScreen extends Component {
+    state = {
+        loaded: false
+    };
+    componentDidMount() {
+        this.initializeIfNeeded().catch(err => {
+            console.warn('Error', err.message);
+        });
+    }
+
+    async initializeIfNeeded() {
+        if (!(await Spotify.isInitializedAsync())) {
+            // init
+            const options = {
+                clientID: '4266b38056c54d47a5480dc099f59cb6',
+                sessionUserDefaultsKey: 'SpotifySession',
+                redirectURL: 'reactnativespotify://auth',
+                scopes: [
+                    'user-read-private',
+                    'playlist-read',
+                    'playlist-read-private',
+                    'streaming'
+                ]
+            };
+
+            const loggedIn = await Spotify.initialize(options);
+
+            if (loggedIn) {
+                this.props.navigation.navigate('Home');
+            } else {
+                this.setState({ loaded: true });
+            }
+        } else {
+            if (await Spotify.isLoggedInAsync()) {
+                this.props.navigation.navigate('Home');
+            } else {
+                this.setState({ loaded: true });
+            }
+        }
+    }
+
+    login() {
+        Spotify.login().then(loggedIn => {
+            if (loggedIn) {
+                this.props.navigation.navigate('Home');
+            } else {
+                console.log('FAILURE');
+            }
+        });
+    }
+
+    render() {
+        return this.state.loaded ? (
+            <View style={styles.container}>
+                <Text style={styles.loginHeader}>Spotify</Text>
+                <TouchableNativeFeedback
+                    style={styles.login}
+                    onPress={this.login}
+                >
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableNativeFeedback>
+            </View>
+        ) : null;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#191414'
+    },
+    loginHeader: {
+        fontSize: 34,
+        color: '#fff',
+        marginBottom: 25,
+        fontWeight: 'bold'
+    },
+    login: {
+        backgroundColor: '#1DB954',
+        width: 120,
+        height: 60,
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    loginText: {
+        color: '#fff'
+    }
+});
