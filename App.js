6ecdbaaf3a62e421fe9616d797d9c258413888f4
idanlo <idan2110@gmail.com>
2019-03-19 15:39:33 +0200

    add basic spotify log in screen

--- App.js
+++ App.js
@@ -1,49 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
+import Spotify from 'rn-spotify-sdk';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+    componentDidMount() {
+        this.initializeIfNeeded().catch(err => {
+            console.warn('Error');
+        });
+    }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    async initializeIfNeeded() {
+        if (!(await Spotify.isInitializedAsync())) {
+            // init
+            const options = {
+                clientID: '4266b38056c54d47a5480dc099f59cb6',
+                sessionUserDefaultsKey: 'SpotifySession',
+                redirectURL: 'spotify://auth',
+                scopes: [
+                    'user-read-private',
+                    'playlist-read',
+                    'playlist-read-private',
+                    'streaming'
+                ]
+            };
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+            const loggedIn = await Spotify.initialize(options);
+
+            if (loggedIn) {
+                console.warn('Success');
+            }
+        } else {
+            if (await Spotify.isLoggedInAsync()) {
+                console.warn('Success');
+            }
+        }
+    }
+
+    login() {
+        Spotify.login().then(loggedIn => {
+            if (loggedIn) {
+                console.warn('SUCCESS');
+            } else {
+                console.error('FAILURE');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.loginHeader}>Spotify</Text>
+                <TouchableNativeFeedback
+                    style={styles.login}
+                    onPress={this.login}
+                >
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableNativeFeedback>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#191414'
+    },
+    loginHeader: {
+        fontSize: 34,
+        color: '#fff',
+        marginBottom: 25,
+        fontWeight: 'bold'
+    },
+    login: {
+        backgroundColor: '#1DB954',
+        width: 120,
+        height: 60,
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    loginText: {
+        color: '#fff'
+    }
 });
