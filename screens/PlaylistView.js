30aa5fa62c1840caede4673c80e41cd3e4fc83de
idanlo <idan2110@gmail.com>
2019-03-28 22:25:14 +0200

    add modal actions to PlaylistView, and a follow button

--- screens/PlaylistView.js
+++ screens/PlaylistView.js
@@ -14,6 +14,7 @@ import Spotify from 'rn-spotify-sdk';
 import Icon from 'react-native-vector-icons/Ionicons';
 import globalStyles from '../styles';
 import Song from '../components/Song';
+import { ModalContext } from '../App';
 
 function PlaylistView(props) {
     const [data, setData] = React.useState(null);
@@ -55,93 +56,134 @@ function PlaylistView(props) {
         <View style={globalStyles.container}>
             <StatusBar backgroundColor="#191414" />
             {data && !loading ? (
-                <ScrollView>
-                    <View style={([globalStyles.container], { paddingTop: 0 })}>
-                        <View style={styles.header}>
-                            <TouchableOpacity
-                                onPress={() => props.navigation.goBack()}
+                <ModalContext.Consumer>
+                    {({ openModal }) => (
+                        <ScrollView>
+                            <View
+                                style={
+                                    ([globalStyles.container],
+                                    { paddingTop: 0 })
+                                }
                             >
-                                <Icon
-                                    name="ios-arrow-back"
-                                    size={30}
-                                    color="#fff"
-                                />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Icon name="md-more" size={30} color="#fff" />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{ alignItems: 'center' }}>
-                            <Image
-                                source={{
-                                    uri: data.images[0].url
-                                }}
-                                style={{ width: 150, height: 150 }}
-                            />
-                            <Text
-                                style={{
-                                    fontWeight: 'bold',
-                                    fontSize: 24,
-                                    textAlign: 'center'
-                                }}
-                            >
-                                {data.name}
-                            </Text>
-                            <Text
-                                style={{
-                                    fontSize: 14,
-                                    textAlign: 'center'
-                                }}
-                                color="grey"
-                            >
-                                Playlist by {data.owner.display_name}
-                            </Text>
-                            <TouchableOpacity
-                                onPress={() => Spotify.playURI(data.uri, 0, 0)}
-                            >
-                                <View
-                                    style={{
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        width: 150,
-                                        height: 50,
-                                        borderRadius: 25,
-                                        backgroundColor: '#1DB954',
-                                        marginTop: 10,
-                                        marginBottom: 10
-                                    }}
-                                >
-                                    <Text
-                                        style={{
-                                            fontSize: 28,
-                                            textAlign: 'center',
-                                            fontWeight: 'bold'
+                                <View style={styles.header}>
+                                    <TouchableOpacity
+                                        onPress={() =>
+                                            props.navigation.goBack()
+                                        }
+                                    >
+                                        <Icon
+                                            name="ios-arrow-back"
+                                            size={30}
+                                            color="#fff"
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            openModal(
+                                                data.images[0].url,
+                                                data.name,
+                                                `Playlist by ${
+                                                    data.owner.display_name
+                                                }`,
+                                                [
+                                                    {
+                                                        text: 'Follow',
+                                                        click: () =>
+                                                            Spotify.sendRequest(
+                                                                `v1/playlists/${
+                                                                    data.id
+                                                                }/followers`,
+                                                                'PUT',
+                                                                {},
+                                                                false
+                                                            )
+                                                    }
+                                                ]
+                                            );
                                         }}
                                     >
-                                        Play
-                                    </Text>
+                                        <Icon
+                                            name="md-more"
+                                            size={30}
+                                            color="#fff"
+                                        />
+                                    </TouchableOpacity>
                                 </View>
-                            </TouchableOpacity>
-                        </View>
+                                <View style={{ alignItems: 'center' }}>
+                                    <Image
+                                        source={{
+                                            uri: data.images[0].url
+                                        }}
+                                        style={{ width: 150, height: 150 }}
+                                    />
+                                    <Text
+                                        style={{
+                                            fontWeight: 'bold',
+                                            fontSize: 24,
+                                            textAlign: 'center'
+                                        }}
+                                    >
+                                        {data.name}
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            fontSize: 14,
+                                            textAlign: 'center'
+                                        }}
+                                        color="grey"
+                                    >
+                                        Playlist by {data.owner.display_name}
+                                    </Text>
+                                    <TouchableOpacity
+                                        onPress={() =>
+                                            Spotify.playURI(data.uri, 0, 0)
+                                        }
+                                    >
+                                        <View
+                                            style={{
+                                                alignItems: 'center',
+                                                justifyContent: 'center',
+                                                width: 150,
+                                                height: 50,
+                                                borderRadius: 25,
+                                                backgroundColor: '#1DB954',
+                                                marginTop: 10,
+                                                marginBottom: 10
+                                            }}
+                                        >
+                                            <Text
+                                                style={{
+                                                    fontSize: 28,
+                                                    textAlign: 'center',
+                                                    fontWeight: 'bold'
+                                                }}
+                                            >
+                                                Play
+                                            </Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
 
-                        <FlatList
-                            contentContainerStyle={{
-                                zIndex: 1000,
-                                flex: 1,
-                                marginHorizontal: 10
-                            }}
-                            scrollEnabled={false}
-                            data={data.tracks.items}
-                            keyExtractor={(_, i) => i.toString()}
-                            renderItem={({ item }) => (
-                                <Song
-                                    song={item.track}
-                                    artists={item.track.artists}
+                                <FlatList
+                                    contentContainerStyle={{
+                                        zIndex: 1000,
+                                        flex: 1,
+                                        marginHorizontal: 10
+                                    }}
+                                    scrollEnabled={false}
+                                    data={data.tracks.items}
+                                    keyExtractor={(_, i) => i.toString()}
+                                    renderItem={({ item }) => (
+                                        <Song
+                                            song={item.track}
+                                            artists={item.track.artists}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                    </View>
-                </ScrollView>
+                            </View>
+                        </ScrollView>
+                    )}
+                </ModalContext.Consumer>
             ) : (
                 <View
                     style={[
