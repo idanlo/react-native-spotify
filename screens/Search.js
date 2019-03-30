8e6614455558280eae5c60d842f98270a2fbb227
idanlo <idan2110@gmail.com>
2019-03-30 18:42:38 +0300

    add some UI for search screen

--- screens/Search.js
+++ screens/Search.js
@@ -1,14 +1,104 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar, ScrollView } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    StatusBar,
+    ScrollView,
+    FlatList,
+    TouchableOpacity,
+    Dimensions,
+    TextInput
+} from 'react-native';
 import { Text } from '../UI';
+import LinearGradient from 'react-native-linear-gradient';
 import globalStyles from '../styles';
 
+const { width } = Dimensions.get('screen');
+
 export default function Search() {
+    console.log(StatusBar.currentHeight);
     return (
-        <View style={globalStyles.container}>
-            <StatusBar backgroundColor="#191414" />
+        <View style={{ backgroundColor: '#191414' }}>
             <ScrollView>
-                <Text style={styles.header}>Search</Text>
+                <LinearGradient
+                    colors={['#fff', '#191414']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 3, y: 3 }}
+                    locations={[0, 0.1]}
+                >
+                    <StatusBar
+                        translucent={true}
+                        backgroundColor={'transparent'}
+                    />
+                    <View style={{ marginTop: 50 }}>
+                        <Text bold size={34} style={styles.header}>
+                            Search
+                        </Text>
+                        <TextInput
+                            onEndEditing={e => console.log(e.nativeEvent.text)}
+                            placeholder="Artists, song or playlists"
+                            placeholderTextColor="#363636"
+                            style={{
+                                width: '75%',
+                                alignSelf: 'center',
+                                backgroundColor: '#F7F7F7',
+                                textAlign: 'center',
+                                borderRadius: 7
+                            }}
+                        />
+                    </View>
+                    <View style={{ marginTop: 30 }}>
+                        <Text
+                            bold
+                            size={20}
+                            style={{ marginBottom: 15, marginLeft: 10 }}
+                        >
+                            Your top genres
+                        </Text>
+                        <FlatList
+                            data={[
+                                { name: 'Rock' },
+                                { name: 'Indie' },
+                                { name: 'Pop' },
+                                { name: 'Metal' }
+                            ]}
+                            numColumns={2}
+                            scrollEnabled={false}
+                            keyExtractor={(_, i) => i.toString()}
+                            contentContainerStyle={{ marginHorizontal: 10 }}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity
+                                    style={{
+                                        flex: 1,
+                                        alignItems: 'center',
+                                        paddingBottom: 20,
+                                        position: 'relative'
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            width: width / 2 - 30,
+                                            height: (width / 2 - 30) / 2,
+                                            backgroundColor: 'blue',
+                                            borderRadius: 6
+                                        }}
+                                    />
+                                    <Text
+                                        bold
+                                        size={16}
+                                        style={{
+                                            position: 'absolute',
+                                            top: 15,
+                                            left: 30
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Text>
+                                </TouchableOpacity>
+                            )}
+                        />
+                    </View>
+                </LinearGradient>
             </ScrollView>
         </View>
     );
@@ -16,7 +106,7 @@ export default function Search() {
 
 const styles = StyleSheet.create({
     header: {
-        fontSize: 34,
-        fontWeight: 'bold'
+        textAlign: 'center',
+        marginBottom: 10
     }
 });
