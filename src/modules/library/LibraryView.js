6a210502e94334fd81efdf17f8199a76d292f716
idanlo <idan2110@gmail.com>
2019-04-20 17:29:17 +0300

    add basic ui for the library screen (links to go to user's playlists, albums etc)

--- src/modules/library/LibraryView.js
+++ src/modules/library/LibraryView.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Text from '../../components/Text';
 import { commonStyles as globalStyles } from '../../styles';
 
@@ -26,8 +27,155 @@ export default class Library extends React.Component {
     render() {
         return (
             <View style={globalStyles.container}>
-                <ScrollView>
-                    <Text style={styles.header}>Library</Text>
+                <ScrollView
+                    contentContainerStyle={{
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text bold size={34}>
+                        Library
+                    </Text>
+                    <>
+                        <View style={styles.item}>
+                            <TouchableOpacity
+                                style={{ flex: 10 }}
+                                onPress={() =>
+                                    this.props.navigation.navigate(
+                                        'PaginationView',
+                                        {
+                                            next:
+                                                'https://api.spotify.com/v1/me/playlists',
+                                        },
+                                    )
+                                }
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Icon
+                                        name="ios-musical-notes"
+                                        size={30}
+                                        color="#fff"
+                                        style={styles.icon}
+                                    />
+                                    <Text
+                                        numberOfLines={1}
+                                        style={{ flex: 10 }}
+                                    >
+                                        Playlists
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.item}>
+                            <TouchableOpacity
+                                style={{ flex: 10 }}
+                                onPress={() =>
+                                    this.props.navigation.navigate(
+                                        'PaginationView',
+                                        {
+                                            next:
+                                                'https://api.spotify.com/v1/me/tracks',
+                                        },
+                                    )
+                                }
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Icon
+                                        name="ios-musical-note"
+                                        size={30}
+                                        color="#fff"
+                                        style={styles.icon}
+                                    />
+                                    <Text
+                                        numberOfLines={1}
+                                        style={{ flex: 10 }}
+                                    >
+                                        Songs
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.item}>
+                            <TouchableOpacity
+                                style={{ flex: 10 }}
+                                onPress={() =>
+                                    this.props.navigation.navigate(
+                                        'PaginationView',
+                                        {
+                                            next:
+                                                'https://api.spotify.com/v1/me/albums',
+                                        },
+                                    )
+                                }
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Icon
+                                        name="ios-albums"
+                                        size={25}
+                                        color="#fff"
+                                        style={styles.icon}
+                                    />
+                                    <Text
+                                        numberOfLines={1}
+                                        style={{ flex: 10 }}
+                                    >
+                                        Albums
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.item}>
+                            <TouchableOpacity
+                                style={{ flex: 10 }}
+                                onPress={() =>
+                                    this.props.navigation.navigate(
+                                        'PaginationView',
+                                        {
+                                            next:
+                                                'https://api.spotify.com/v1/me/following?type=artist',
+                                        },
+                                    )
+                                }
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Icon
+                                        name="ios-person"
+                                        size={25}
+                                        color="#fff"
+                                        style={styles.icon}
+                                    />
+                                    <Text
+                                        numberOfLines={1}
+                                        style={{ flex: 10 }}
+                                    >
+                                        Artists
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </>
                 </ScrollView>
             </View>
         );
@@ -38,5 +186,18 @@ const styles = StyleSheet.create({
     header: {
         fontSize: 34,
         fontWeight: 'bold',
+    },
+    item: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50,
+        marginHorizontal: 10,
+        justifyContent: 'space-between',
+    },
+    icon: {
+        marginRight: 10,
+        flex: 1,
+        textAlign: 'center',
     },
 });
